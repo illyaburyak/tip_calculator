57586efc95b27d54250f15add8f5aa3c7072f0be
Illya <illia.buryak@anoda.mobi>
2021-09-17 14:15:27 -0500

    Initial Commit

---
+++ FirstYTProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FirstYTProject
+//
+//  Created by Illya  on 9/17/21.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+   
+
+   var body: some View {
+    Text("Hello")
+   }
+   
+   
+}
+
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+      ContentView()
+   }
+}
---
+++ FirstYTProject/FirstYTProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstYTProjectApp.swift
+//  FirstYTProject
+//
+//  Created by Illya  on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstYTProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
