6f60055029ab3a81117ef60c4983eb0350411527
Illya <illia.buryak@anoda.mobi>
2021-09-17 14:38:42 -0500

    "Tip calculator"

--- FirstYTProject/ContentView.swift
+++ FirstYTProject/ContentView.swift
@@ -9,10 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var  checkAmount = ""
+   @State private var  numberOfPeople = 2
+   @State private var tipPrecentage = 0
    
-
+   let tipPrecentages = [0, 5, 10, 15, 20]
+   
+   var totalPerPerson: Double {
+      let peopleCount = Double(numberOfPeople + 2)
+      let tipSelection = Double(tipPrecentages[tipPrecentage])
+      let orderAmount = Double(checkAmount) ?? 0
+      
+      let tipValue = orderAmount / 100 * tipSelection
+      
+      let grandTotal = orderAmount + tipValue
+      
+      let amountPerPerson = grandTotal / peopleCount
+      
+      return amountPerPerson
+   }
+   
    var body: some View {
-    Text("Hello")
+      NavigationView{
+         Form {
+            Section {
+               TextField("Amount",text: $checkAmount)
+               Picker("Number of people", selection: $numberOfPeople) {
+                  ForEach(2..<100) {
+                     Text("\($0) people")
+                  }
+               }
+            }
+            Section(header: Text("How many tips do you want to leave?")) {
+               Picker("Tip precentage", selection: $tipPrecentage) {
+                  ForEach(0..<tipPrecentages.count) {
+                     Text("\(self.tipPrecentages[$0])")
+                  }
+               }
+               .pickerStyle(SegmentedPickerStyle())
+            }
+            Section{
+               Text("\(totalPerPerson, specifier: "%.2f")")
+            }
+         }
+         .navigationBarTitle("Tip Calculator", displayMode: .inline)
+      }
    }
    
    
